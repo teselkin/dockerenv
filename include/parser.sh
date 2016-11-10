f66b7da54b89b7b0e4be80de1d2d3a74ec055f51
Dmitry Teselkin <dteselkin@mirantis.com>
2016-11-10 14:14:32 +0300

    Fix setup-user macro

--- include/parser.sh
+++ include/parser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+set -o xtrace
 parser=$(mktemp)
-parser_out=$(mktemp)
 cat << 'EOF' > ${parser}
 /^uid/ {
   uid = $2
@@ -18,23 +18,41 @@ cat << 'EOF' > ${parser}
   }
 }
 END {
-  groups = ""
-  for (k in arr) {
-    print "groupadd --gid " k " " arr[k]
-    if (k != gid) {
-      if (groups == "") {
-        groups = k
-      } else {
-        groups = groups "," k
-      }
-    }
+  print "gid " gid_name " " gid
+  print "uid " uid_name " " uid
+  for (id in arr) {
+    print "group " arr[id] " " id
   }
-  print "useradd --uid " uid " --gid " gid " --groups " groups " " uid_name
-  print "echo '" uid_name " ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/" uid_name
-  print "chmod 440 /etc/sudoers.d/" uid_name
 }
 EOF
-echo "${ID}" | tr '[:blank:]' '\n' | tr '=(),' ' ' | awk -f ${parser} > ${parser_out}
-source ${parser_out}
+echo "${ID}" | tr '[:blank:]' '\n' | tr '=(),' ' ' | awk -f ${parser} > ${parser}.out
+
+user_name=
+user_id=
+group_name=
+group_id=
+while read key name id; do
+  case ${key} in
+    'uid')
+      user_id=${id}
+      user_name=${name}
+      useradd --create-home --uid ${user_id} --gid ${group_id} ${user_name}
+    ;;
+    'gid')
+      group_id=${id}
+      group_name=${name}
+      groupadd --gid ${group_id} ${group_name}
+    ;;
+    'group')
+      if groupadd --gid ${id} ${name}; then
+        usermod -a -G ${id} ${user_name}
+      fi
+    ;;
+  esac
+done < ${parser}.out
+
+echo "${user_name} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${user_name}
+chmod 440 /etc/sudoers.d/${user_name}
+
 rm ${parser}
-rm ${parser_out}
+rm ${parser}.out
